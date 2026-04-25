f5e5322fbbb077b1c11c773ab1151d54101b0c24
Your Name <thiago.bafi@outlook.com>
2026-04-25 00:17:18 -0300

    cadastro backend

--- usedevolva2/src/main/java/com/devolva/use/Application.java
+++ usedevolva2/src/main/java/com/devolva/use/Application.java
@@ -2,7 +2,7 @@ package com.devolva.use;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//AAAAAAAAAAAAAAAAAAAAAAAAAA
+
 @SpringBootApplication
 public class Application {
 
