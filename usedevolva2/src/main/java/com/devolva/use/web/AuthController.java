82d68b1bb72fc082549409eb25bed40e7129369f
Your Name <thiago.bafi@outlook.com>
2026-04-29 02:16:34 -0300

    add route to edit tool

--- usedevolva2/src/main/java/com/devolva/use/web/AuthController.java
+++ usedevolva2/src/main/java/com/devolva/use/web/AuthController.java
@@ -1,7 +1,9 @@
 package com.devolva.use.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class AuthController {
@@ -39,6 +41,11 @@ public class AuthController {
     @GetMapping("/users/create-tool")
     public String createTool() {
         return "users/create-tool";
+    }
+    @GetMapping("/users/edit-tool/{id}")
+    public String editToolPage(@PathVariable Long id, Model model) {
+        model.addAttribute("toolId", id);
+        return "users/edit-tool";
     }
 
 }
