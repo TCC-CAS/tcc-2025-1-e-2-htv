0d86fb001b8a0835fe7330075c07ba4ae5acfe8b
Thiago <105989334+ThiagoGatti@users.noreply.github.com>
2026-05-22 19:51:18 -0300

    criar admin mockado

--- usedevolva2/src/main/java/com/devolva/use/Application.java
+++ usedevolva2/src/main/java/com/devolva/use/Application.java
@@ -1,7 +1,11 @@
 package com.devolva.use;
 
+import com.devolva.use.security.dtos.AdminDto;
+import com.devolva.use.security.usecases.AdminUsecases;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -9,4 +13,25 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
+
+	@Bean
+    CommandLineRunner initAdmin(AdminUsecases adminUsecases) {
+		return args -> {
+			try {
+				AdminDto novoAdmin = new AdminDto(
+						"admin@usedevolva.com",
+						"admin123",
+						"Admin Geral",
+						true
+				);
+
+				adminUsecases.createAdmin(novoAdmin);
+				System.out.println("✅ ADMINISTRADOR CRIADO COM SUCESSO NO JAVALAND!");
+			} catch (IllegalArgumentException e) {
+				System.out.println("ℹ️ Admin já estava cadastrado: " + e.getMessage());
+			}
+		};
+	}
+
 }
