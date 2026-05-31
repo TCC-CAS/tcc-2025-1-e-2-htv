11288e02dbb53946bec707d629c6b2465a45d0ec
Thiago <105989334+ThiagoGatti@users.noreply.github.com>
2026-05-31 07:53:24 -0300

    alteração de fonte

--- usedevolva2/src/main/java/com/devolva/use/Application.java
+++ usedevolva2/src/main/java/com/devolva/use/Application.java
@@ -9,7 +9,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
-
+import com.devolva.use.security.dtos.AdminDto;
+import com.devolva.use.security.usecases.AdminUsecases;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
 @SpringBootApplication
@@ -19,5 +25,27 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Bean
+	CommandLineRunner initAdmin(AdminUsecases adminUsecases) {
+		return args -> {
+			try {
+				AdminDto defaultAdmin = new AdminDto(
+						"admin@admin.com",
+						"admin123",
+						"Administrador Padrão",
+						true
+				);
 
+				adminUsecases.createAdmin(defaultAdmin);
+				System.out.println("======> Administrador padrão criado com sucesso! <======");
+
+			} catch (IllegalArgumentException e) {
+				System.out.println("======> Admin padrão já existente: " + e.getMessage() + " <======");
+			} catch (Exception e) {
+				System.err.println("Erro ao tentar criar o admin padrão: " + e.getMessage());
+			}
+		};
+	}
 }
+
+
